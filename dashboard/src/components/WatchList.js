eea058b6220a953544459cb1d2d69452307e7dd7
nallasaiteja <nallasaitejagupta@gmail.com>
2025-08-11 12:09:24 +0530

    structured WatchList

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import { Tooltip, Grow } from "@mui/material";
+
+import {tooltip,Grow} from "@mui/material";
+import { watchlist } from "../data/data";
+
 const WatchList = () => {
   return (
     <div className="watchlist-container">
@@ -11,10 +16,15 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length}/ 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+           return <WatchListItem stock={stock} key={index} />;
+          })}         
+
+      </ul>
     </div>
   );
 };
