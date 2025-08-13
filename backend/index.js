19aacd6d73355a0f3e76d815e97e45551c333aa8
nallasaiteja <nallasaitejagupta@gmail.com>
2025-08-13 16:27:54 +0530

    Connected to Mangodb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
+
+
+const PORT =  process.env.PORT || 3002;
+
+const url = process.env.MANGO_URL;
 
 const app = express();
 
-app.listen(3002, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
+  mongoose.connect(url)
 });
